a0bb03a912b5648cd41c194f25540acfe7be4a97
Abdullah <abdullah.alioglu@bahcesehir.edu.tr>
2024-06-24 15:44:14 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Header";
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const handlePlusClick = () => {
+    setStep((s) => s + 1);
+  };
+  const handleMinusClick = () => {
+    setStep((s) => (s > 1 ? s - 1 : 1));
+  };
+
+  const handleCountPlusClick = () => {
+    setCount((c) => c + step);
+  };
+
+  const handleCountMinusClick = () => {
+    setCount((c) => c - step);
+  };
+
+  const date = new Date("Jun 24 2024");
+  date.setDate(date.getDate() + count);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Header />
       </header>
-    </div>
+      <div className="context">
+        <div className="step">
+          <button onClick={handleMinusClick}>-</button>
+          <p>Step: {step}</p>
+          <button onClick={handlePlusClick}>+</button>
+        </div>
+        <div className="count">
+          <button onClick={handleCountMinusClick}>-</button>
+          <p>Count: {count}</p>
+          <button onClick={handleCountPlusClick}>+</button>
+        </div>
+        <div className="text">
+          <p>
+            {count === 0 && `Today is ${date.toDateString()}`}
+            {count > 0 && `${count} days from today is ${date.toDateString()}`}
+            {count < 0 &&
+              `${Math.abs(count)} days ago was ${date.toDateString()}`}
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function Header() {
+  return (
+    <>
+      <h1 className="header">Date Counter</h1>
+    </>
+  );
+}
